76329e2ed08b4754898013919a2f1dff314f1445
Evan <75786458+EmrysChoo@users.noreply.github.com>
2025-02-20 23:01:15 +0800

    Update TMDB.js

--- TMDB.js
+++ TMDB.js
@@ -1,10 +1,14 @@
 const url = $request.url;
 const newUrl = new URL(url);
 
-// 修改语言为中文
-newUrl.searchParams.set('language', 'zh-CN');
+// 仅针对需要参数的路径（如搜索、发现、电影/剧集详情）
+const needModify = /\/3\/(discover|search|movie|tv)\//.test(url);
 
-// 解锁成人内容
-newUrl.searchParams.set('include_adult', 'true');
+if (needModify) {
+  // 修改语言为中文
+  newUrl.searchParams.set('language', 'zh-CN');
+  // 解锁成人内容
+  newUrl.searchParams.set('include_adult', 'true');
+}
 
 $done({ url: newUrl.toString() });
