e6c4863ff44425cdef92beb0ed70e7ccb97be02c
Evan <75786458+EmrysChoo@users.noreply.github.com>
2025-02-20 23:08:45 +0800

    Update TMDB.js

--- TMDB.js
+++ TMDB.js
@@ -1,10 +1,10 @@
 const url = $request.url;
 const newUrl = new URL(url);
 
-// 仅针对需要参数的路径（如搜索、发现、电影/剧集详情）
-const needModify = /\/3\/(discover|search|movie|tv)\//.test(url);
+// 排除 /images 路径
+const isImageRequest = /\/3\/tv\/\d+\/images/.test(url);
 
-if (needModify) {
+if (!isImageRequest) {
   // 修改语言为中文
   newUrl.searchParams.set('language', 'zh-CN');
   // 解锁成人内容
